c3d1d3b6cef05788da597fe2308abfc464f89946
angeldesigner <angeltae26@gmail.com>
2020-07-30 22:06:04 -0500

    Updates

---
+++ precache-manifest.543f7b70be55efbaa792868f6f3f609c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff0f5b241ff86e785f8ccfdd57da277",
+    "url": "/miappenreact/index.html"
+  },
+  {
+    "revision": "392d15dac491e342b0ff",
+    "url": "/miappenreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/miappenreact/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miappenreact/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392d15dac491e342b0ff",
+    "url": "/miappenreact/static/js/main.d98f03b5.chunk.js"
+  },
+  {
+    "revision": "b60f2858db7f94dbee0d",
+    "url": "/miappenreact/static/js/runtime-main.0dfdd14a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miappenreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
